d80e481f50dc07a558801458115e460080dfe8f2
aseaboyer <aseaboyer@gmail.com>
2014-04-28 00:01:40 -0700

    rework the truck inPlay system

--- js/game.js
+++ js/game.js
@@ -126,25 +126,29 @@ function pickupTruck() {
 /* Classes */
 function Truck() {
     return {
-        residence: null, // which the tile the truck is on.
+        inPlay: true, // if the truck is on a tile
 		x: 0, // tile in where it resides
 		y: 0,
-		held: false,
+		held: false, // if the player is 'holding' the truck
 		place: function(newX,newY) {
 			this.x = newX;
 			this.y = newY;
+			this.inPlay = true;
 			this.held = false;
 		},
 		pickup: function() {
 			this.x = newX;
 			this.y = newY;
+			this.inPlay = false;
 			this.held = true;
 		},
 		drawTruck: function(board) {
-			if(this.residence != null) {
+			if(this.inPlay != true) {
 				board.fillStyle = "#900";
 				board.fillRect( (this.x + (game.tileSize.x * 0.25)), (this.y + (game.tileSize.y * 0.25)),
 					(game.tileSize.x * 0.5), (game.tileSize.y * 0.5) );
+			} else {
+				// figure out how we are going to tray it
 			}
 		}
     };
