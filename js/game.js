3f1984b3ab980484adf6b1e5f340d7533477bf01
aseaboyer <aseaboyer@gmail.com>
2014-04-28 22:02:57 -0700

    un simplify

--- js/game.js
+++ js/game.js
@@ -77,8 +77,9 @@ Start();
 setInterval( mainloop, ONE_FRAME_TIME );
 
 function loadLevel(levelNum) {
-	console.log("Fetching : levels/level"+levelNum+".json")
-	var holder = ajax_get_json("levels/level"+levelNum+".json")
+	var holder = ajax_get_json("levels/level"+levelNum+".json");
+	console.log("Fetching : levels/level"+levelNum+".json");
+	console.log(holder);
 	
 	return holder;
 }
