5f965562f017ac2202f2e4641b57706c6fad743c
aseaboyer <aseaboyer@gmail.com>
2014-05-03 12:12:55 -0700

    set vals

--- js/game.js
+++ js/game.js
@@ -259,10 +259,21 @@ function Tile(tileX, tileY, tileType, spriteX, spriteY) { // Tiles should own th
             board.fillRect(this.x, this.y, game.tileSize.x, game.tileSize.y);
 			//draw that image sprite
 			board.drawImage(spriteTileImg,
-				64, 64, // top corner loc of sub - this is the sub offset locs
-				64, 64, // w/ of sub - needs to be full image size
-				10,10, // top corner on canvas
-				64,64); // width of canvas - the size of the sprite on the canvas
+				spriteDX, spriteDY, // top corner loc of sub - this is the sub offset locs
+				game.tileSize.x, game.tileSize.y, // w/ of sub - needs to be full image size
+				this.x, this.y, // top corner on canvas
+				game.tileSize.x,game.tileSize.y); // width of canvas - the size of the sprite on the canvas
+				
+				/*
+				The above works!
+				TOP CORNERS ARE SET by DX/DY VAL
+				Leave the width of the sub and canvas THE SAME
+				TOP CORNER IS THE X/Y
+				Leave the width of the sub and canvas THE SAME
+				*/
+				
+				
+				
 			/*board.drawImage(spriteTileImg,
 				this.x, this.y,
 				game.tileSpritesheet.x, game.tileSpritesheet.y,
