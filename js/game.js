2ae7fd88ffe69318cb42bb3a441aa0a391976c29
aseaboyer <aseaboyer@gmail.com>
2014-05-02 22:31:17 -0700

    typos

--- js/game.js
+++ js/game.js
@@ -262,7 +262,7 @@ function Tile(tileX, tileY, tileType, spriteX, spriteY) { // Tiles should own th
 				64, 64, // top corner loc of sub - this is the sub offset locs
 				576, 576, // w/ of sub - needs to be full image size
 				this.x, this.y, // top corner on canvas
-				game.tileSpriteSheet.x,game.tileSpriteSheet.y); // width of canvas - the size of the sprite on the canvas
+				game.tileSpritesheet.x, game.tileSpritesheet.y); // width of canvas - the size of the sprite on the canvas
 			/*board.drawImage(spriteTileImg,
 				this.x, this.y,
 				game.tileSpritesheet.x, game.tileSpritesheet.y,
