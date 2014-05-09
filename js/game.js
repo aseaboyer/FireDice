e2483dfee17a52d563d0281222fa7dfca7548c7f
aseaboyer <aseaboyer@gmail.com>
2014-05-08 21:33:32 -0700

    more logging

--- js/game.js
+++ js/game.js
@@ -75,10 +75,11 @@ var game = {
 		}
 		
 		console.log("There are " + validDropPoints.length + " possible starting tiles for a truck.");
-		var spawnPoint = validDropPoints[Math.floor((Math.random() * validDropPoints.length) + 1)];
+		var optionPicked = Math.floor((Math.random() * validDropPoints.length) + 1)
+		var spawnPoint = validDropPoints[optionPicked];
 		if(validDropPoints.length > 0) { // find valid drop point
 			aTruck.spawn( spawnPoint );
-			console.log("Spawning a new truck the following points -");
+			console.log("Spawning a new truck the following points - option:"+optionPicked);
 			console.log(spawnPoint);
 		}
 	},
