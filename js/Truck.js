1c233dd0b0298b9c73659ba7fc7ee42e47a798e7
aseaboyer <aseaboyer@gmail.com>
2014-05-19 23:30:47 -0700

    changing array creation

--- js/Truck.js
+++ js/Truck.js
@@ -20,12 +20,11 @@ function Truck(trayVals) {
 			this.held = false;
 			
 			// update the hosing vals (4 compass points)
-			this.truckHosing = {
-				[x: this.x+1, y: this.y],
-				[x: this.x-1, y: this.y],
-				[x: this.x, y: this.y+1],
-				[x: this.x, y: this.y-1]
-			};
+			this.truckHosing = 
+				[{x: this.x+1, y: this.y}],
+				[{x: this.x-1, y: this.y}],
+				[{x: this.x, y: this.y+1}],
+				[{x: this.x, y: this.y-1}];
 		},
 		pickup: function() {
 			this.held = true;
