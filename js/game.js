8581a0dc1e4d39906234b0cde23fce3933ae02f5
aseaboyer <aseaboyer@gmail.com>
2014-05-20 22:06:09 -0700

    commented out the console log

--- js/game.js
+++ js/game.js
@@ -11,7 +11,7 @@ var game = {
 		update: function() {
 			this.lastFrame = this.thisFrame;
 			this.thisFrame = new Date().getTime();
-			console.log(this.getFrameDiff());
+			//console.log(this.getFrameDiff());
 		},
 		getCurrentTime: function() {
 			return new Date().getTime();
