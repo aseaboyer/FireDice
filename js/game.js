4427cfc69e4f48fabbd80667fc2d18e77b28f03c
aseaboyer <aseaboyer@gmail.com>
2014-05-22 23:35:13 -0700

    adding a test?

--- js/game.js
+++ js/game.js
@@ -111,6 +111,7 @@ var game = {
 			for(var i=0; i < listCount; i++) {
 				console.log(listCount[i].number + "} " + listCount[i].name);
 			}
+			console.log("Ran drawLevelArray");
 				/*
 				b.font = "bold 24px Arial";
 				b.textAlign = 'right';
