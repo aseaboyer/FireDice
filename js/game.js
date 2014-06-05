400dc2555a2784442d2cba04ac89ac7dde158667
aseaboyer <aseaboyer@gmail.com>
2014-06-04 23:09:10 -0700

    flip xy in for loops?

--- js/game.js
+++ js/game.js
@@ -248,16 +248,16 @@ var game = {
 		
 		console.log("New Tiles:");
 		
-		for(var x=0; x < tileCols; x++) {
-			var tileRows = tileData[x].length;
-			for(var y=0; y < tileRows; y++) {
+		for(var y=0; y < tileCols; y++) {
+			var tileRows = tileData[y].length;
+			for(var x=0; x < tileRows; x++) {
 				var isHouseStart = isTruckStart = false;
 				if(tileData[x][y].truckStart) { isTruckStart = true; }
 				if(tileData[x][y].houseStart) { isHouseStart = true; }
 				
 				// Should change the way the tile saves now as well
 				
-				var newTile = Tile(y, x, tileData[x][y].type,
+				var newTile = Tile(x, y, tileData[x][y].type,
 					tileData[x][y].spriteX, tileData[x][y].spriteY,
 					isTruckStart, isHouseStart,
 					game.tileSize.x, game.tileSize.y);
